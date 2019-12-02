d6c06488b89472ed8de43d229392e8d7f8d2fe28
Alex Burbano (RIT Student) <arb8590@ad.rit.edu>
2019-12-02 10:45:04 -0500

    auntie

--- sprite.js
+++ sprite.js
@@ -1,5 +1,27 @@
 class sprite{
     constructor(){
+        this.radius;
+        this.speed;
+        this.location;
+        this.state;
+        this.stateIndex;
+        this.stateImages;
+    }
+    isWithin(){
         
     }
+    updateIndex(){
+
+    }
+    display(){
+
+    }
+    transition(){
+
+    }
+    move(){
+
+    }
+
+        
 }
--- spriteManager.js
+++ spriteManager.js
@@ -2,6 +2,37 @@ class SpriteManager{
     constructor(){
         this.sprites=[]
         this.images = new Map();
-        this.states
+        this.states = {
+            fall : 0,
+            left : 1,
+            right : 2,
+            drag : 3
+        }
+
     }
+    loadSpriteImages(){
+
+    }
+    addSprite(){
+
+    }
+    getLength(){
+
+    }
+    clearAll(){
+
+    }
+    transitionAll(){
+
+    }
+    display(){
+
+    }
+    moveAll(){
+
+    }
+    filterOutliers(){
+
+    }
+
 }
